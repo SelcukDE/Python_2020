ca696671c77fe21920ea4a2b0e10d6dab7c49667
Alisami <alisamisvater@gmail.com>
2020-07-09 17:51:12 +0200

    yeni yerine yollanıyor

---
+++ deposit.py
@@ -0,0 +1,9 @@
+deposit = 1000 # ilk gün yatırdığımız para - pazartesi
+deposit = deposit * 7/100 + deposit # ilk günün parası - salı
+deposit = deposit * 7/100 + deposit # ikinci günün parası - çarşamba
+deposit = deposit * 7/100 + deposit # üçüncü günün parası - perşembe
+deposit = deposit * 7/100 + deposit # dördüncü günün parası - cuma
+deposit = deposit * 7/100 + deposit # beşinci günün parası - cumartesi
+deposit = deposit * 7/100 + deposit # altıncı günün parası - pazar
+deposit = deposit * 7/100 + deposit # yedinci günün parası - pazartesi
+deposit
